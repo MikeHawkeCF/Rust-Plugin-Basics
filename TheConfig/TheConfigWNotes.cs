d2f733d78582cf884ce6aeccdabb4ce4139a94a1
Mike Hawke <73037912+MikeHawkeCF@users.noreply.github.com>
2020-10-17 22:26:28 +0100

    Update TheConfigWNotes.cs

--- TheConfig/TheConfigWNotes.cs
+++ TheConfig/TheConfigWNotes.cs
@@ -86,7 +86,7 @@ namespace Oxide.Plugins
             //E.g. if we did "SaveConfig(cheese);" config would equal "cheese"
 
             //We are refenceing that here
-            //Config.WritObject (write and object in the config folder
+            //Config.WriteObject (write and object in the config folder
             //(config, true) config = configData... or cheese and true as in go ahead
             Config.WriteObject(config, true);
 
